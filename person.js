64e784f2e386c2a70cbd19c7d1fb19103e939053
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-06-25 10:22:42 +0700

    Membuat Virtual Property Di Mongoose

---
+++ person.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/ShopApp")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const personSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+});
+
+personSchema.virtual("fullName").get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  firstName: "Harry",
+  lastName: "Potter",
+});
+
+console.log(person.fullName);
